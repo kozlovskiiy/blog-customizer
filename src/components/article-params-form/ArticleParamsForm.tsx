d9666c6658bc7917b841e2d51675e692a5c01b75
Stanislav Kozlovskiy <stan.kozlowscky@yandex.ru>
2025-02-16 18:56:12 +0300

    review gotovo

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,53 +1,65 @@
-import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
-import { Select } from 'src/ui/select';
-import { RadioGroup } from 'src/ui/radio-group';
-import { Separator } from 'src/ui/separator'; 
-import { Text } from 'src/ui/text';
-import clsx from 'clsx';
+import { useEffect, useRef, useState } from "react";
+import { ArticleStateType, OptionType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions } from "src/constants/articleProps";
+import { ArrowButton } from "src/ui/arrow-button";
+import { Button } from "src/ui/button";
+import { Select } from "src/ui/select";
+import { RadioGroup } from "src/ui/radio-group";
+import { Separator } from "src/ui/separator";
+import { Text } from "src/ui/text";
+import clsx from "clsx";
+import styles from "./ArticleParamsForm.module.scss";
 
-import styles from './ArticleParamsForm.module.scss';
-import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from '../../constants/articleProps';
-import { OptionType } from 'src/constants/articleProps';
+export const ArticleParamsForm: React.FC<{ setArticleState: (state: ArticleStateType) => void }> = ({ setArticleState }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [font, setFont] = useState<OptionType>(fontFamilyOptions[0]);
+  const [fontSize, setFontSize] = useState<OptionType>(fontSizeOptions[0]);
+  const [fontColor, setFontColor] = useState<OptionType>(fontColors[0]);
+  const [backgroundColor, setBackgroundColor] = useState<OptionType>(backgroundColors[0]);
+  const [contentWidth, setContentWidth] = useState<OptionType>(contentWidthArr[0]);
 
-interface ArticleParamsFormProps {
-  onClick: () => void;
-  isOpen: boolean;
-  font: OptionType;
-  setFont: (font: OptionType) => void;
-  fontSize: OptionType;
-  setFontSize: (font: OptionType) => void;
-  fontColor: OptionType;
-  setFontColor: (font: OptionType) => void;
-  backgroundColor: OptionType;
-  setBackgroundColor: (font: OptionType) => void;
-  contentWidth: OptionType;
-  setContentWidth: (font: OptionType) => void;
-  onSubmit: (event: React.FormEvent) => void;
-  onReset: () => void;
-}
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setArticleState({
+      fontFamilyOption: font,
+      fontSizeOption: fontSize,
+      fontColor,
+      backgroundColor,
+      contentWidth,
+    });
+  };
 
-export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({ 
-  onClick,
-  isOpen,
-  font,
-  setFont,
-  setFontSize,
-  fontSize,
-  fontColor,
-  setFontColor,
-  backgroundColor,
-  setBackgroundColor,
-  contentWidth,
-  setContentWidth,
-  onSubmit,
-  onReset
-}) => {
+  const handleReset = () => {
+    setFont(defaultArticleState.fontFamilyOption);
+    setFontSize(defaultArticleState.fontSizeOption);
+    setFontColor(defaultArticleState.fontColor);
+    setBackgroundColor(defaultArticleState.backgroundColor);
+    setContentWidth(defaultArticleState.contentWidth);
+    setArticleState(defaultArticleState);
+  };
+
+
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target as HTMLElement)) {
+      setIsSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.addEventListener("click", handleClickOutside);
+      return () => {
+        document.removeEventListener("click", handleClickOutside);
+      };
+    }
+  }, [isSidebarOpen, handleClickOutside]);
+    
   return (
     <>
-      <ArrowButton isOpen={isOpen} onClick={onClick} />
-      <aside className={clsx(styles.container, { [styles.container_open]: isOpen })}>
-        <form className={styles.form} onSubmit={onSubmit}>
+      <ArrowButton isOpen={isSidebarOpen} onClick={(e) => { setIsSidebarOpen(!isSidebarOpen), e.stopPropagation() }} />
+      <aside ref={sidebarRef} className={clsx(styles.container, { [styles.container_open]: isSidebarOpen })} onClick={(e) => e.stopPropagation()} >
+      <form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
           <Text children="Задайте параметры" weight={800} size={31} family="open-sans" uppercase />
           <Select title="Шрифт" selected={font} options={fontFamilyOptions} onChange={setFont} />
           <RadioGroup title="Размер шрифта" name="font-size" options={fontSizeOptions} selected={fontSize} onChange={setFontSize} />
@@ -56,7 +68,7 @@ export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({
           <Select title="Цвет фона" selected={backgroundColor} options={backgroundColors} onChange={setBackgroundColor} />
           <Select title="Ширина контента" selected={contentWidth} options={contentWidthArr} onChange={setContentWidth} />
           <div className={styles.bottomContainer}>
-            <Button title="Сбросить" htmlType="reset" type="clear" onClick={onReset} />
+            <Button title="Сбросить" htmlType="reset" type="clear"  />
             <Button title="Применить" htmlType="submit" type="apply" />
           </div>
         </form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,101 +1,38 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
-
-import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from './constants/articleProps';
-import { OptionType } from './constants/articleProps';
+import { CSSProperties, StrictMode, useState } from "react";
+import { ArticleStateType, defaultArticleState } from "./constants/articleProps";
+import { Article } from "./components/article/Article";
+import { ArticleParamsForm } from "./components/article-params-form/ArticleParamsForm";
+import styles from "./styles/index.module.scss";
+import clsx from "clsx";
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  const [font, setFont] = useState<OptionType>(fontFamilyOptions[0]);
-  const [fontSize, setFontSize] = useState<OptionType>(fontSizeOptions[0]);
-  const [fontColor, setFontColor] = useState<OptionType>(fontColors[0]);
-  const [backgroundColor, setBackgroundColor] = useState<OptionType>(backgroundColors[0]);
-  const [contentWidth, setContentWidth] = useState<OptionType>(contentWidthArr[0]);
+  const [articleState, setArticleState] = useState<ArticleStateType>(defaultArticleState);
 
-  const [appliedSettings, setAppliedSettings] = useState({
-    font: fontFamilyOptions[0],
-    fontSize: fontSizeOptions[0],
-    fontColor: fontColors[0],
-    backgroundColor: backgroundColors[0],
-    contentWidth: contentWidthArr[0],
-  });
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    setAppliedSettings({
-      font,
-      fontSize,
-      fontColor,
-      backgroundColor,
-      contentWidth,
-    });
-  };
-  console.log(contentWidth)
-
-
-	const handleReset = () => {
-		const defaultSettings = {
-			font: fontFamilyOptions[0],
-			fontSize: fontSizeOptions[0],
-			fontColor: fontColors[0],
-			backgroundColor: backgroundColors[0],
-			contentWidth: contentWidthArr[0],
-		};
-
-		setFont(defaultSettings.font);
-		setFontSize(defaultSettings.fontSize);
-		setFontColor(defaultSettings.fontColor);
-		setBackgroundColor(defaultSettings.backgroundColor);
-		setContentWidth(defaultSettings.contentWidth);
-
-		setAppliedSettings(defaultSettings);
-	};
   return (
     <main
       className={clsx(styles.main)}
       style={
         {
-          '--font-family': appliedSettings.font.value,
-          '--font-size': appliedSettings.fontSize.value,
-          '--font-color': appliedSettings.fontColor.value,
-          '--container-width': appliedSettings.contentWidth.value,
-          '--bg-color': appliedSettings.backgroundColor.value,
+          '--font-family': articleState.fontFamilyOption.value,
+          '--font-size': articleState.fontSizeOption.value,
+          '--font-color': articleState.fontColor.value,
+          '--container-width': articleState.contentWidth.value,
+          '--bg-color': articleState.backgroundColor.value,
         } as CSSProperties
       }
     >
-      <ArticleParamsForm
-        onClick={() => setVisible(!visible)}
-        isOpen={visible}
-        font={font}
-        setFont={setFont}
-        fontSize={fontSize}
-        setFontSize={setFontSize}
-        fontColor={fontColor}
-        setFontColor={setFontColor}
-        backgroundColor={backgroundColor}
-        setBackgroundColor={setBackgroundColor}
-        contentWidth={contentWidth}
-        setContentWidth={setContentWidth}
-        onSubmit={handleSubmit} 
-        onReset={handleReset} 
-      />
+      <ArticleParamsForm setArticleState={setArticleState} />
       <Article />
     </main>
   );
 };
-
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
